d249640b3e27f9166d9a02eed8bc6dfb0301abff
jeremytkearney <jeremy.t.kearney@gmail.com>
2022-05-05 16:19:30 -0500

    Added basic slash commands

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require necessary discord.js dependencies
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Interaction } = require('discord.js');
 const { token } = require("./config.json");
 
 // Create new client instance
@@ -8,6 +8,21 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // Initialization code, only run once
 client.once('ready', () => {
     console.log('Jellybot is ready to go!');
+});
+
+// Listen for interactions and reply to commands
+client.on('interactionCreate', async interaction => {    
+    if(!Interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if(commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if(commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if (commandName === 'user') {
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    }
 });
 
 // Login bot to Discord using client token
