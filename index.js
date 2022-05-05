c6334d38bcc9ddb5450b82a5837c7c60163dbc3a
jeremytkearney <49045925+jeremytkearney@users.noreply.github.com>
2022-05-05 17:29:38 -0500

    Setup (#2)
    * Initial setup for js discord bot
    
    * Delete config.json
    
    * Delete process.env
    
    * Added basic slash commands
    
    * Added command handling
    
    * Added basic command handling support for slash commands in index.js and deploy-commands.js
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Created event handler and dynamic command refreshing
    
    * Missed a spot

--- index.js
+++ index.js
@@ -2,50 +2,32 @@
 const fs = require('node:fs');
 const { Client, Intents, Interaction } = require('discord.js');
 const { token } = require("./config.json");
+const { REST } = require('@discordjs/rest');
 
 // Create new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// Initialization code, only run once
-client.once('ready', c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-// Command handler
-client.commands = new Collection();
-// Read in all files in jellybot-js/commands/ with a .js extension
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-// Iterate through every file and set the command from the file
-for(const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    client.commands.set(command.data.name, command);
-}
 
 // Events handler
 const eventFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for(const file  of eventFiles) {
-    const command = require(`./commands/${file}`);
+    const event = require(`./events/${file}`);
+    if(event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
 }
 
-// Listens for and executes commands
-client.on('interactionCreate', async interaction => {
-    if(!interaction.isCommand()) return;
+// Command handler
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    const command =  client.commands.get(interaction.commandName);
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
 
-    if(!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch(error) {
-        console.error(error);
-        await interactions.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-});
+    client.commands.set(command.data.name, command);
+}
 
 // Login bot to Discord using client token
 client.login(token);
